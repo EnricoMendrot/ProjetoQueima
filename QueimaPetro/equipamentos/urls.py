dd99331684b8dfb6be966c3737e56de4d3867d62
Enrico Mendrot <enricomendrot23@gmail.com>
2025-11-09 10:55:11 -0300

    Atualização do Projeto: -- Sistema de Cadastro de Equipamento finalizado -- Sistema de visualização de Equipamento finalizado -- Integração total com o banco de dados

--- QueimaPetro/equipamentos/urls.py
+++ QueimaPetro/equipamentos/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = 'equipamento'
+
 urlpatterns = [
-    path("", views.exibicaoequipamento),
-    path("cadastrar/", views.cadastroequipamento),
+    path("", views.exibicaoequipamento, name = 'visualizacao'),
+    path("cadastrar/", views.cadastroequipamento, name='cadastro'),
 ]
--- QueimaPetro/equipamentos/views.py
+++ QueimaPetro/equipamentos/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .forms import EquipamentoForm
+from database.models import Equipamento
 # Create your views here.
 
 def exibicaoequipamento(request):
-    return HttpResponse("Olá")
+    contexto = {
+        "equipamento": Equipamento.objects.all()
+    }
+    return render(request, 'Visualizacao/VisualizacaoEquipamento.html', contexto)
 
 def cadastroequipamento(request):
     if request.method == "POST":
         formulario = EquipamentoForm(request.POST)
         if formulario.is_valid():
             formulario.save()
-            return redirect("operadores:visualizacao")
+            return redirect("equipamentos:visualizacao")
         
     contexto = {
         'form': EquipamentoForm
